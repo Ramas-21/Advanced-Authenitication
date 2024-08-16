833e98db99c999e4fdf71c300ebc035b3d5ff7fa
lawrence <lawrencehome21@gmail.com>
2024-08-16 07:15:05 +0300

    created the models file

--- backend/routes/authentication.routes.js
+++ backend/routes/authentication.routes.js
@@ -4,9 +4,7 @@ import { login, logout, signup } from "../controllers/authentication.controllers
 const router = express.Router()
 
 router.get('/signup', signup)
-
 router.get('/login', login)
-
 router.get('/logout', logout)
 
 export default router;
