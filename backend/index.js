0acff7f1273f3bccd3a5cbdd872d9db6aac7240d
lawrence <lawrencehome21@gmail.com>
2024-08-16 07:13:01 +0300

    created controllers and their routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/connectDB.js';
+import authenticationRoutes from './routes/authentication.routes.js'
 dotenv.config();
 
 
 const app = express();
+
+app.use("/api/authentication", authenticationRoutes)
+
 app.listen(3000, () => {
     connectDB();
     console.log("server is running on port 300")
