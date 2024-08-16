dee6cb9aa6a81d8e375d0f99d04b512573513681
lawrence <lawrencehome21@gmail.com>
2024-08-16 06:46:53 +0300

    connected to the database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/connectDB.js';
+dotenv.config();
+
 
 const app = express();
 app.listen(3000, () => {
+    connectDB();
     console.log("server is running on port 300")
 })
 
