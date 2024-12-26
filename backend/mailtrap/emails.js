aa37a1458229bf7139b0db6b1b810dc8395be6eb
Your Name <lawrencehome21@gmail.com>
2024-12-26 17:51:53 +0300

    integrated ApI for sending emails

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -19,3 +19,24 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error(`Error sending verification email: ${error}`);
     }
 }
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "f9a69249-fad0-4e76-98bf-fbdf4460c6a9",
+            template_variables: {
+                company_info_name: "Auth Company",
+                name: name
+            }
+        })
+
+        console.log("Welcome email sent successfully", response);
+    } catch (error) {
+        console.error('Error sending welcome email', error);
+        throw new Error(`Error sending welcome email: ${error}`);
+    }
+}
