9bc46816816daa8fdd42935d27f507167ad21015
Your Name <lawrencehome21@gmail.com>
2024-12-27 20:22:59 +0300

    created input elements

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Input = ({icon:Icon, ...props}) => {
+  return (
+    <div className='relative mb-6'>
+        <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+            <Icon className="size-5 text-blue-500"/>
+        </div>
+    </div>
+  )
+}
+
+export default Input
